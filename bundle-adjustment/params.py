7d07f935a383ffdbf38736b51cd7e6a4bfced61c
kemfic <19ficici.kemal@gmail.com>
2018-12-29 15:08:58 -0500

    started working on ba

---
+++ bundle-adjustment/params.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+from skimage.transform import EssentialMatrixTransform as T
+
+
+param = dict(
+	shi_tomasi = dict(
+		maxCorners = 10000,
+		qualityLevel = 0.01,
+		minDistance = 7),
+
+	ransac_params = dict(
+        model_class = T,
+		min_samples = 8,
+		residual_threshold = 0.01,
+		max_trials = 100
+		)
+
+)
